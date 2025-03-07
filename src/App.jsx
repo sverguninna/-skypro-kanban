1ebebcc5d37c810d527ce8f4bff176497c01f0b4
@sverguninna <sverguninna6@gmail.com>
2025-03-07 22:51:06 +0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import PopNewCard from './components/PopNewCard/PopNewCard'
+import PopExit from './components/PopExit/PopExit'
+import Header from './components/Header/header'
+import Column from './components/column/Column'
+import Modal from './components/Modal/Modal'
+import PopBrowse from './components/PopBrowse/PopBrowse'
+import Registration from './components/Registration/Registration'
+import './App.css'
+import './signin.css'
+import './signup.css'
+
+function App() {
+  return (
+    <>
+           <div className="wrapper">
+     
+		<Modal/>
+		<Registration/>
+      <PopExit/>
+      <PopNewCard/>
+	  <PopBrowse/>
+      <Header/>
+	  <Column/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Test.jsx
@@ -0,0 +1,15 @@
+// react component это функция возврвщвющая jsx разметку
+// react component должен возвращать 1 элемент
+
+import './Test.css'
+
+ function Test () {
+    return (
+       <>
+         <h1 className='title'>Test</h1>
+         <p>tggg</p>
+       </>
+    )
+}
+
+export default Test
